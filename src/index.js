c5428d2c9f8ea9e39128fdbd8fade2792eb0fd13
shanemacbride <shanemacb@gmail.com>
2018-04-25 14:51:18 -0700

    debug dynamo

--- src/index.js
+++ src/index.js
@@ -80,7 +80,8 @@ slack.on('/beerjar', (msg, bot) => {
     // If the first character is @, slice it off
     msg.text = (msg.text[0] === '@') ? msg.text.slice(1) : msg.text;
     //let message = { text: `beerjar ${msg.text}!` };
-    let message = { text: `get: ${self.store.get(msg.text).catch()}`};
+    let result = self.store.get(msg.text);
+    let message = { text: `get: ${result}`};
     bot.reply(message); 
   }
 });
